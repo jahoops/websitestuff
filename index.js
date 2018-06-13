9190e5a759ee3302a326a9b6f8615c59154c9a57
jahoops <jwhooper@gmail.com>
2018-06-12 21:52:29 -0400

    up

--- index.js
+++ index.js
@@ -1,99 +1,82 @@
-  var rendering = false; // window resize event waits until this is false to request a render
-  var firetransitions = false; // transitions only work on a change, like width, so we need to call render twice
-  function render() {
-    firetransitions = rendering; // if the rendering flag is true coming in, fire transitions
-    rendering = true; // set rendering flag so that the resize event does not keep calling this function
+  var divqueue = [];
+  var body = document.getElementsByTagName("BODY")[0];
+  var rendering = false;
+  
+  function init() {
+    rendering = true;
     var element_ids = ['header', 'leftColumn', 'rightColumn', 'footer']; // names of my index.html divs
     // if this is the first pass
-    if (!firetransitions) {
-      for (var e = 0; e < element_ids.length; e++) {
-        var body = document.getElementsByTagName("BODY")[0];
-        var container_div = document.getElementById(element_ids[e]);
-        container_div.innerHTML = ""; // clear contents
-        var height = container_div.offsetHeight;
-        var width = container_div.offsetWidth;
-        // create twelve divs and put them inside this content div
-        for (var i = 0; i < 12; i++) {
-          var div_to_insert = document.createElement('div');
-          div_to_insert.style.transition = 'left 0.5s ease'; // width transition
-          div_to_insert.style.transition += ',width 0.3s ease'; // width transition
-          div_to_insert.style.height = height + 'px';
-          div_to_insert.style.width = '1px'; // set to 1 for now, next pass will resize and set off transition
-          div_to_insert.style.position = 'absolute';
-          div_to_insert.style.left = (i * width / 12) + 'px';
-          div_to_insert.setAttribute('original-left',(i * width / 12));
-          div_to_insert.style.backgroundColor = randomColor(); // see randomColor function at bottom
-          //div_to_insert.style.border = 'solid thin lightgray';
-          body.appendChild(div_to_insert);
-        }
-      } 
-      // NOW, call a second pass, because that will allow our new elements to render on the page
-      // requestAnimationFrame waits for the browser to finish rendering between frames
-      requestAnimationFrame(render);
-    } else {
-      // THIS is the second pass, to fire transitions
-      for (var e = 0; e < element_ids.length; e++) {
-        var container_div = document.getElementById(element_ids[e]);
-        var width = container_div.offsetWidth;
-        var get_divs = document.querySelectorAll('#' + element_ids[e] + ' div'); // get all the divs inside this element
-        for (var i = 0; i < get_divs.length; i++) {
-          get_divs[i].style.width = width / 12 + 'px'; // now we change the width, and the transition will fire
-          get_divs[i].addEventListener('mousedown', function(e) {
-            var f = function(){
-              var srcElement = e.srcElement;
-              if(!e.shiftKey) {
-                srcElement.style.left = ((parseInt(srcElement.style.left, 10) || 0) + 50) + 'px';
-              } else {
-                gohomeElement = srcElement;
-                gohomeElement.style.transition = "none !important";
-                var d = new Date();
-                gohometick = d.getTime() + 2000; // two seconds from now
-                requestAnimationFrame( function(){ gohome(gohomeElement,gohometick) } ); // back to original-left
-              }
-            };
-            requestAnimationFrame(f);
-          });
-        }
+    for (var e = 0; e < element_ids.length; e++) {
+      var container_div = document.getElementById(element_ids[e]);
+      container_div.innerHTML = ""; // clear contents
+      var height = container_div.offsetHeight;
+      var width = container_div.offsetWidth;
+      // create twelve divs and put them inside this content div
+      for (var i = 0; i < 12; i++) {
+        var div_to_insert = document.createElement('div');
+        div_to_insert.classList.add('can-move');
+        div_to_insert.style.transition = 'left 0.5s ease'; // width transition
+        div_to_insert.style.transition += ',width 0.3s ease'; // width transition
+        div_to_insert.style.height = height + 'px';
+        div_to_insert.style.width = (width / 12) + 'px';
+        div_to_insert.style.position = 'absolute';
+        div_to_insert.style.left = (i * width / 12) + 'px';
+        div_to_insert.style.backgroundColor = randomColor(); // see randomColor function at bottom
+        //div_to_insert.style.border = 'solid thin lightgray';
+        container_div.appendChild(div_to_insert);
       }
-      rendering = false;
     } 
+    rendering = false;
   }
 
-  function gohome(gohomeElement,gohometick) {
+  function moveTo(element,arrivePosition,arriveTick,movePerTick) {
+    rendering = true;
     var d = new Date();
     var ticks = d.getTime();
-    if (ticks < gohometick) {
-      var leftnow = parseInt(gohomeElement.style.left, 10) || 0; // the || sets to 0 if it can't parse to an integer
-      var leftorig = gohomeElement.getAttribute('original-left');
-      var x = leftnow - (leftnow - leftorig)/(gohometick-ticks);
-      gohomeElement.style.left = x + 'px';
-      requestAnimationFrame( function(){ gohome(gohomeElement,gohometick) } );
-    } else {
-      gohomeElement.style.left = gohomeElement.getAttribute('original-left') + 'px';
+    var elapsed = arriveTick - ticks;
+    if(!movePerTick) {
+      var top = parseInt(gohomeElement.style.top, 10) || 0;
+      var left = parseInt(gohomeElement.style.left, 10) || 0;
+      movePerTick = { top: (arrivePosition.top - top)/elapsed, left: (arrivePosition.left - left)/elapsed };
+      requestAnimationFrame( function(){ moveTo(element,arrivePosition,arriveTick,movePerTick) } );
     }
-  }
-
-  function moveTo(element,toPosition,fromPosition,arriveTick,speedFunction,pathFunction) {
-    var d = new Date();
-    var ticks = d.getTime();
-    if (ticks < gohometick) {
-      var leftnow = parseInt(gohomeElement.style.left, 10) || 0; // the || sets to 0 if it can't parse to an integer
-      var leftorig = gohomeElement.getAttribute('original-left');
-      var x = leftnow - (leftnow - leftorig)/(gohometick-ticks);
-      gohomeElement.style.left = x + 'px';
-      requestAnimationFrame( function(){ gohome(gohomeElement,gohometick) } );
+    if (elapsed > 0) {
+      element.style.top += (movePerTick.top * elapsed) + 'px';
+      element.style.left += (movePerTick.left * elapsed) + 'px';
+      requestAnimationFrame( function(){ moveTo(element,arrivePosition,arriveTick,movePerTick) } );
     } else {
-      gohomeElement.style.left = gohomeElement.getAttribute('original-left') + 'px';
+      element.style.top = arrivePosition.top + 'px';
+      element.style.left = arrivePosition.left + 'px';
     }
+    rendering = false;
   }
 
 
   // initial render
-  requestAnimationFrame(render);
+  requestAnimationFrame(init);
 
-  // don't fire render if already rendering
+  var classname = document.getElementsByClassName("can-move");
+
+  var myFunction = function() {
+    divqueue.push(this);
+    if(divqueue.length>1) {    
+      var d = new Date();
+      var ticks = d.getTime();    
+      var el1 = divqueue.pop();
+      var el2 = divqueue.pop();
+      requestAnimationFrame(function(){
+        moveTo(el1, { top: el2.style.top, left:el2.style.left }, ticks+1000);
+        moveTo(el2, { top: el1.style.top, left:el1.style.left }, ticks+1000);
+      });
+    }
+  };
+  
+  for (var i = 0; i < classname.length; i++) {
+      classname[i].addEventListener('click', myFunction, false);
+  }
+
   window.addEventListener('resize', function () {
-    if (!rendering) requestAnimationFrame(render);
+    if (!rendering) requestAnimationFrame(init);
   });
 
 
